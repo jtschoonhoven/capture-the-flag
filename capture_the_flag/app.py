fc130500ad6b004755652423b7bd978d0f10d3d4
jtschoonhoven <jschoonhoven@lyft.com>
2017-08-02 23:38:45 -0700

    support file upload

--- capture_the_flag/app.py
+++ capture_the_flag/app.py
@@ -4,8 +4,8 @@ from flask_restful import Api
 
 from capture_the_flag import database as db
 from capture_the_flag.models.user import load_user_from_session, User
-from capture_the_flag.session import SuperSecureSessionInterface
 from capture_the_flag.routes import files, index, login, logout, signup, users
+from capture_the_flag.session import SuperSecureSessionInterface
 
 
 # HACK: don't serve static files from the root path
@@ -15,33 +15,52 @@ api = Api(app)
 app.config.from_object(__name__)
 # HACK: this is a terrible secret key
 app.config['SECRET_KEY'] = 'secret'
+app.config['MAX_CONTENT_LENGTH'] = 256 * 1024  # 256kb
 
 # HACK: don't use compromised session interface
 app.session_interface = SuperSecureSessionInterface()
 
+# configure setup/teardown of DB connection
 app.before_request(db.connect)
 app.teardown_appcontext(db.close)
 
+# initialize DB with an admin user
 db.database.create_tables([User], safe=True)
 User.get_or_create(
     username='admin',
+    # HACK: don't hardcode admin credentials
     defaults={'username': 'admin', 'password': 'admin', 'file_access_path': '*'}
 )
 
+# configure login manager
 login_manager = flask_login.LoginManager()
 login_manager.login_view = "login"
 login_manager.login_message = u"Please log in."
 login_manager.init_app(app)
 login_manager.user_loader(load_user_from_session)
 
+# define routes
 app.add_url_rule('/', 'index', index.handle_request)
 app.add_url_rule('/login', 'login', login.handle_request, methods=['GET', 'POST'])
 app.add_url_rule('/logout', 'logout', logout.handle_request)
 app.add_url_rule('/signup', 'signup', signup.handle_request, methods=['GET', 'POST'])
-app.add_url_rule('/files/', 'files', files.handle_request, defaults={'path': 'shared'})
-app.add_url_rule('/files/<path:path>', 'files', files.handle_request)
 app.add_url_rule('/users', 'users', users.handle_request)
+
+app.add_url_rule(
+    '/files/',
+    'files',
+    files.handle_request,
+    defaults={'path': 'shared'},
+    methods=['GET', 'POST'],
+)
+app.add_url_rule(
+    '/files/<path:path>',
+    'files',
+    files.handle_request,
+    methods=['GET', 'POST'],
+)
 
 
 if __name__ == '__main__':
+    # HACK: never run an app in debug mode
     app.run(debug=True)
--- capture_the_flag/routes/files.py
+++ capture_the_flag/routes/files.py
@@ -4,28 +4,45 @@ import flask
 import flask_login
 from typing import List  # noqa
 
-
-ROOT_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+from capture_the_flag.constants import ROOT_PATH
 
 
 @flask_login.login_required
 def handle_request(path):
+    # type: (str) -> str
     if flask.request.method == 'GET':
         return get(path)
+    if flask.request.method == 'POST':
+        return post(path)
+
     flask.flash('method not allowed')
     return flask.redirect(flask.url_for('index'))
 
 
-def is_authorized(path):
-    # type: (str) -> bool
-    # HACK: this is a terrible way to do ACL
-    user_path = flask_login.current_user.file_access_path
+def post(path):
+    # type: (str) -> str
+    file = flask.request.files.get('file')
 
-    if user_path == '*':
-        return True
-    if 'private' in path:
-        return False
-    return True
+    if not file or not file.filename:
+        flask.flash('request did not contain a file')
+        return flask.redirect(flask.request.url)
+
+    if not is_authorized(path):
+        flask.flash('you are not authorized to upload to this directory')
+        return flask.redirect(flask.request.url)
+
+    # HACK: always whitelist file extensions for uploads
+    # HACK: always use werkzeug.utils.secure_filename to sanitize file names
+    abs_path = os.path.join(ROOT_PATH, path, file.filename)
+
+    try:
+        file.save(abs_path)
+    except Exception as e:
+        # HACK: don't display raw error message to user
+        flask.flash('upload failed: "{}"'.format(str(e)))
+
+    flask.flash('Upload successful.')
+    return flask.redirect(flask.request.url)
 
 
 def get(path):
@@ -83,3 +100,15 @@ def get(path):
         path_urls=path_urls,
         is_authorized=is_authorized(path),
     )
+
+
+def is_authorized(path):
+    # type: (str) -> bool
+    # HACK: this is a terrible way to do ACL
+    user_path = flask_login.current_user.file_access_path
+
+    if user_path == '*':
+        return True
+    if 'private' in path:
+        return False
+    return True
